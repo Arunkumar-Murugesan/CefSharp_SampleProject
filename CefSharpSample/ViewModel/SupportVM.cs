99a49989660868a4627f48342b2259964e157578
ROOT\arunkumarmurugesan <arunkumarmurugesan@syncfusion.com>
2018-02-13 11:12:08 +0530

    CefSharpSample

---
+++ CefSharpSample/ViewModel/SupportVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharpSample.ViewModel
+{
+    public class SupportVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(String propertyName)
+        {
+            // verify that the property name matches a real,  
+            // public, instance property on this Object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                Debug.Fail("Invalid property name: " + propertyName);
+            }
+        }
+
+        /// </summary>
+        private string _updateHistory = "https://www.google.co.in";
+        public string UpdateHistory
+        {
+            get { return _updateHistory; }
+            set
+            {
+                this._updateHistory =  value;
+                this.RaisePropertyChanged("UpdateHistory");
+            }
+        }
+    }
+}
